85946aa9f6c20c7d48b96ac2a59feab72c89b2dc
Prashant Kumar <kr.prashantjsr@gmail.com>
2023-03-12 21:44:22 +0530

    excerise

--- javascript-basics/script.js
+++ javascript-basics/script.js
@@ -61,3 +61,59 @@ let order2 = calculateTotal(85, 95);
 let order3 = calculateTotal(85, 65);
 
 console.log(order1, order2, order3);
+
+// Object
+
+const car = {
+  maker: "mercedes",
+  model: "S class",
+  year: 2016,
+  colors: ["red", "black", "white"],
+  hybrid: true,
+  drive: function () {
+    console.log("drive");
+  },
+  stop: function () {
+    console.log("stop");
+  },
+};
+
+console.log(car.maker);
+console.log(car.colors[0]);
+car.drive();
+car.stop();
+
+// Conditional
+
+const person1 = {
+  name: "Prashant",
+  age: 20,
+  status: "resident",
+};
+
+const person2 = {
+  name: "Sagar",
+  age: 24,
+  status: "tourist",
+};
+
+if (person1.age >= 18 && person1.status === "resident") {
+  console.log("you can cast a vote");
+} else {
+  console.log("you can not cast a vote");
+}
+
+if (person2.age >= 18 && person2.status === "resident") {
+  console.log("you can cast a vote");
+} else {
+  console.log("you can not cast a vote");
+}
+
+// String
+
+function fullName({ firstName, lastName }) {
+  return `${firstName} ${lastName}`;
+}
+
+let resultName = fullName({ lastName: "Kumar", firstName: "Prashant" });
+console.log(resultName.toUpperCase());
